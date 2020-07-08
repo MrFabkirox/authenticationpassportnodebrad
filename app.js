3f9bb80adcff908aeb2604716edd0bb9f591b808
mrfabkirox <fabrice.esope@gmail.com>
2020-07-08 08:42:34 +0800

    after mongo

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts')
 
-const routeIndex = require('./routes/index')
-const app = express()
+require('dotenv').config()
 
+// Mongo
+const mongoose = require("mongoose");
+const uri = process.env.ATLAS_URI
+
+mongoose.connect(uri, {
+//    dbname: 'tigernodesandreact',
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, function(error) {
+    if(error) {
+      console.log(
+        "Error connecting to mongo [%o]", error);
+    }
+  }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB atlas connected");
+})
+
+// route
+const routeIndex = require('./routes/index')
+
+const app = express()
 
 // app.use('/public',
 //   express.static(
